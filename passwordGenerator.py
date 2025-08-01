7f332781725a48d6e763a51b3f71c36f51879be5
Kamil Wysocki <79612956+WysockiKamil@users.noreply.github.com>
2025-08-01 23:57:19 +0200

    password generator

---
+++ passwordGenerator.py
@@ -0,0 +1,9 @@
+import random
+
+def generate_password(length):
+    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+
+print(generate_password(12))
